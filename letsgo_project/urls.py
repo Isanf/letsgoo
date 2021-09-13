7de3ba8830597f12505c511a5abe8434c07ba04a
isanf <issak.sanf@gmail.com>
2021-09-13 23:33:35 +0200

    Update urls.py

--- letsgo_project/urls.py
+++ letsgo_project/urls.py
@@ -4,9 +4,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-admin.site.site_header = "Viral Video Admin Panel"
-admin.site.site_title = "Viral Video Admin Portal"
-admin.site.index_title = "Welcome to Video Viral!"
+admin.site.site_header = "LetsGoo Admin Panel"
+admin.site.site_title = "LetsGoo Admin Portal"
+admin.site.index_title = "Welcome to LetsGoo!"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
